4ac0e762af6b79f8eb7881772a1702e9bf547cce
Areebkh <64961559+Areebkh@users.noreply.github.com>
2020-07-01 02:37:46 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello Dua</h1>
+      <h1>Hello from Dua and Areeb to all Czns in group</h1>
     </div>
   );
 }
